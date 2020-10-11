3ae3ff758d692bab6b226ad3248bc00f8dc3d85c
용진 양 <dyyjkd@naver.com>
2020-10-11 05:24:04 +0000

    handling cors

--- back-end/api/app.js
+++ back-end/api/app.js
@@ -22,6 +22,20 @@ app.use(bodyParser.urlencoded({
   // parse application/json
 app.use(bodyParser.json())
 
+// HANDLING CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // routes들 적용
 app.use("/admin", adminRoutes);
 app.use(authRoutes);
