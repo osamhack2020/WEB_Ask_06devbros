8a41380570ff8dcd95c5d3729269ab216013fae6
NEM-NE <dlatqdlatq@mju.ac.kr>
2020-10-12 14:07:02 +0000

    푸터 완성

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,33 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 
 const styles = () => ({
-  legal: {
-    backgroundColor: '#fff',
-    position: 'static',
-    bottom: 0,
-    width: '97.6%',
-    borderTop: '1px solid #eee',
-    padding: '15px',
-    overflow: 'hidden',
-    textAlign: 'center',
-  },
+    legal: {
+        backgroundColor: '#f7f7f7',
+        position: 'static',
+        bottom: 0,
+        width: '97.6%',
+        borderTop: '1px solid #eee',
+        padding: '15px',
+        overflow: 'hidden',
+        textAlign: 'center',
+    },
+    container: {
+        marginBottom: '0.5%',
+        marginTop:'1%',
+    },
+    copyright: {
+        marginTop:'1%',
+        fontSize:'16px'
+    }
 });
 const Footer = (props) => {
-  const { classes } = props;
+    const { classes } = props;
 
-  return (
-    <div className={classes.legal}>
-      <div className="copyright">
-        © 2016 - 2017 <a href="https://github.com/Bikranshu">Krishna Prasad Timilsina</a>
-      </div>
-    </div>
-  );
+    return (
+        <div className={classes.legal}>
+            <Grid container className={classes.container} spacing={2}>
+
+                <Grid item xs={3}>
+                    <Link href="#" color="inherit">
+                        <strong>소개</strong>
+                </Link>
+                </Grid>
+                <Grid item xs={3}>
+                    <Link href="#" color="inherit">
+                    <strong>커뮤니티</strong>
+                </Link>
+                </Grid>
+                <Grid item xs={3}>
+                    <Link href="#" color="inherit">
+                    <strong>고민 답변하기</strong>
+                </Link>
+                </Grid>
+                <Grid item xs={3}>
+                    <Link href="#" color="inherit">
+                    <strong>고객센터</strong>
+                </Link>
+                </Grid>
+            </Grid>
+            <Divider variant="middle" />
+            <div className={classes.copyright}>
+                © 2020 물어봐, Inc. All rights reserved <a href="https://github.com/osamhack2020/WEB_Ask_06devbros">06devbros</a>
+            </div>
+        </div>
+    );
 };
 
 Footer.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Footer);
