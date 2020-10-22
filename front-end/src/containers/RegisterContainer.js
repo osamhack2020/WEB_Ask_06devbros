f7007f738b1ea253081cf17a8a8e5a20b7fbe71c
NEM-NE <dlatqdlatq@mju.ac.kr>
2020-10-22 13:08:41 +0000

    http 관련 js파일 생성

---
+++ front-end/src/containers/RegisterContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { USERS } from '../constants/entity';
+import * as authService from '../service/authService';
+
+// Import custom components
+import RegisterForm from '../components/Form/RegisterForm';
+
+class RegisterContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  /**
+   * Submit the form.
+   *
+   * @param {object} formProps
+   */
+  submitForm(formProps) {
+    this.props.actions.submitForm(USERS, formProps);
+  }
+
+  render() {
+    return <RegisterForm onSubmit={this.submitForm} errorMessage={this.props.errorMessage} />;
+  }
+}
+
+/**
+ * Map the state to props.
+ */
+const mapStateToProps = (state) => ({
+  token: state.auth.token,
+  isAuthenticated: state.auth.isAuthenticated,
+  errorMessage: state.auth.errorMessage,
+});
+
+/**
+ * Map the actions to props.
+ */
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(Object.assign({}, authService), dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterContainer);
