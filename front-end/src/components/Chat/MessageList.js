d1ebce7996b73bc72d3af00afe29464d08503d73
NEM-NE <dlatqdlatq@mju.ac.kr>
2020-10-30 14:52:34 +0000

    favicon 추가

--- front-end/src/components/Chat/MessageList.js
+++ front-end/src/components/Chat/MessageList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Compose from './Compose';
@@ -12,206 +12,122 @@ const MY_USER_ID = 'apple';
 const useStyles = makeStyles((theme) => ({
     message_list_container: {
         padding: '10px',
-        paddingBottom: '70px'
+        paddingBottom: '70px',
+        minHeight: '780px',
     },
     btn: {
         marginLeft: theme.spacing(2),
     }
 }));
 
-export default function MessageList(props) {
+const tempMessages = [
+    {
+        id: 1,
+        author: 'orange',
+        message: '우리나라를 위해 힘쓰시는 장병분들 반갑습니다. 물어봐 AI 챗봇입니다. 어떤 고민이 있으신가요?',
+        timestamp: new Date().getTime()
+    },
+    {
+        id: 2,
+        author: 'apple',
+        message: '요즘 선임 분중에 한 분이 저를 계속 괴롭히고 있어요...',
+        timestamp: new Date().getTime()
+    },
+    {
+        id: 3,
+        author: 'orange',
+        message: '어떻게 괴롭힘 당하고 있나요?',
+        timestamp: new Date().getTime()
+    },
+]
+
+function MessageList(props) {
     const classes = useStyles();
-  const [messages, setMessages] = useState([])
+    const [messages, setMessages] = useState([])
 
-  useEffect(() => {
-    getMessages();
-  },[])
+    useEffect(() => {
+        getMessages();
+    }, [])
 
-  
-  const getMessages = () => {
-     var tempMessages = [
-        {
-          id: 1,
-          author: 'apple',
-          message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 2,
-          author: 'orange',
-          message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 3,
-          author: 'orange',
-          message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 4,
-          author: 'apple',
-          message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 5,
-          author: 'apple',
-          message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 6,
-          author: 'apple',
-          message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 7,
-          author: 'orange',
-          message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 8,
-          author: 'orange',
-          message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 9,
-          author: 'apple',
-          message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-          timestamp: new Date().getTime()
-        },
-        {
-          id: 10,
-          author: 'orange',
-          message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-          timestamp: new Date().getTime()
-        },
-        {
-            id: 11,
-            author: 'orange',
-            message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 12,
-            author: 'orange',
-            message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 13,
-            author: 'orange',
-            message: 'It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 14,
-            author: 'orange',
-            message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 15,
-            author: 'orange',
-            message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 16,
-            author: 'orange',
-            message: 'Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.',
-            timestamp: new Date().getTime()
-          },
-      ]
-      setMessages([...messages, ...tempMessages])
-  }
-
-  const renderMessages = () => {
-    let i = 0;
-    let messageCount = messages.length;
-    let tempMessages = [];
-
-    while (i < messageCount) {
-      let previous = messages[i - 1];
-      let current = messages[i];
-      let next = messages[i + 1];
-      let isMine = current.author === MY_USER_ID;
-      let currentMoment = moment(current.timestamp);
-      let prevBySameAuthor = false;
-      let nextBySameAuthor = false;
-      let startsSequence = true;
-      let endsSequence = true;
-      let showTimestamp = true;
-
-      if (previous) {
-        let previousMoment = moment(previous.timestamp);
-        let previousDuration = moment.duration(currentMoment.diff(previousMoment));
-        prevBySameAuthor = previous.author === current.author;
-        
-        if (prevBySameAuthor && previousDuration.as('hours') < 1) {
-          startsSequence = false;
-        }
-
-        if (previousDuration.as('hours') < 1) {
-          showTimestamp = false;
-        }
-      }
-
-      if (next) {
-        let nextMoment = moment(next.timestamp);
-        let nextDuration = moment.duration(nextMoment.diff(currentMoment));
-        nextBySameAuthor = next.author === current.author;
-
-        if (nextBySameAuthor && nextDuration.as('hours') < 1) {
-          endsSequence = false;
-        }
-      }
-
-      tempMessages.push(
-        <Message
-          key={i}
-          isMine={isMine}
-          startsSequence={startsSequence}
-          endsSequence={endsSequence}
-          showTimestamp={showTimestamp}
-          data={current}
-        />
-      );
-
-      // Proceed to the next message.
-      i += 1;
+    const getMessages = () => {
+        setMessages([...messages, ...tempMessages])
     }
 
-    return tempMessages;
-  }
+    const renderMessages = () => {
+        let i = 0;
+        let messageCount = messages.length;
+        let tempMessages = [];
 
-    return(
-      <div className="message-list">
-        <Toolbar
-          title="AI 고민 상담 대화방"
-          rightItems={[
-            <ToolbarButton key="info" icon="ion-ios-information-circle-outline" />,
-            <ToolbarButton key="video" icon="ion-ios-videocam" />,
-            <ToolbarButton key="phone" icon="ion-ios-call" />
-          ]}
-        />
+        while (i < messageCount) {
+            let previous = messages[i - 1];
+            let current = messages[i];
+            let next = messages[i + 1];
+            let isMine = current.author === MY_USER_ID;
+            let currentMoment = moment(current.timestamp);
+            let prevBySameAuthor = false;
+            let nextBySameAuthor = false;
+            let startsSequence = true;
+            let endsSequence = true;
+            let showTimestamp = true;
 
-        <div className={classes.message_list_container}>{renderMessages()}</div>
+            if (previous) {
+                let previousMoment = moment(previous.timestamp);
+                let previousDuration = moment.duration(currentMoment.diff(previousMoment));
+                prevBySameAuthor = previous.author === current.author;
 
-        <Compose rightItems={[
-            <Button
-            variant="contained"
-            color="primary"
+                if (prevBySameAuthor && previousDuration.as('hours') < 1) {
+                    startsSequence = false;
+                }
 
-            className={classes.btn}
-            >
-             보내기
-            </Button>
-        ]}/>
-      </div>
+                if (previousDuration.as('hours') < 1) {
+                    showTimestamp = false;
+                }
+            }
+
+            if (next) {
+                let nextMoment = moment(next.timestamp);
+                let nextDuration = moment.duration(nextMoment.diff(currentMoment));
+                nextBySameAuthor = next.author === current.author;
+
+                if (nextBySameAuthor && nextDuration.as('hours') < 1) {
+                    endsSequence = false;
+                }
+            }
+
+            tempMessages.push(
+                <Message
+                    key={i}
+                    isMine={isMine}
+                    startsSequence={startsSequence}
+                    endsSequence={endsSequence}
+                    showTimestamp={showTimestamp}
+                    data={current}
+                />
+            );
+
+            // Proceed to the next message.
+            i += 1;
+        }
+
+        return tempMessages;
+    }
+
+    return (
+        <div className="message-list">
+            <Toolbar
+                title="AI 고민 상담 대화방"
+                rightItems={[
+                    <ToolbarButton key="info" icon="ion-ios-information-circle-outline" />,
+                    <ToolbarButton key="video" icon="ion-ios-videocam" />,
+                    <ToolbarButton key="phone" icon="ion-ios-call" />
+                ]}
+            />
+
+            <div className={classes.message_list_container}>{renderMessages()}</div>
+
+            <Compose />
+        </div>
     );
 }
+
+export default MessageList;
